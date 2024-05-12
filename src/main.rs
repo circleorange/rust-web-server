7b3cf918f52badd8824d2f8078fd6fa4b141eaa7
circleorange <piotrbielski3@gmail.com>
2024-05-12 14:47:24 +0100

    404 page for invalid request

--- src/main.rs
+++ src/main.rs
@@ -24,23 +24,43 @@ fn handle_connection(mut stream: TcpStream)
     // `read` takes in mutable reference to self 
     stream.read(&mut buffer).unwrap();
 
-    // convert slice of bytes to string
-    println!("request: {}", String::from_utf8_lossy(&buffer[..]));
+    // hard-coded request we are expecting
+    // `b`` prefix will return byte array representing string
+    let get = b"GET / HTTP/1.1\r\n";
 
-    // to return index.html, contents need to be read into variable
-    let contents = fs::read_to_string("index.html").unwrap();
-    /*
-    Response needs to contain:
-    - HTTP-Version Status-Code Reason-Phrase CRLF
-    - Content-Length header (amount of bytes returned in message-body)
-    - message-body
-    - e.g. HTTP/1.1 200 OK\r\n\r\n
-    */
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    if buffer.starts_with(get)
+    {
+        // convert slice of bytes to string
+        println!("request: {}", String::from_utf8_lossy(&buffer[..]));
+
+        // to return index.html, contents need to be read into variable
+        let contents = fs::read_to_string("index.html").unwrap();
+        /*
+        Response needs to contain:
+        - HTTP-Version Status-Code Reason-Phrase CRLF
+        - Content-Length header (amount of bytes returned in message-body)
+        - message-body
+        - e.g. HTTP/1.1 200 OK\r\n\r\n
+        */
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+    else
+    {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
