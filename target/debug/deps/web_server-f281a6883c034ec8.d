46f7cd80c8859fdda6f12c9431f601f246122731
circleorange <piotrbielski3@gmail.com>
2024-05-12 12:22:35 +0100

    inital commit of skeleton server

---
+++ target/debug/deps/web_server-f281a6883c034ec8.d
@@ -0,0 +1,5 @@
+/home/pbielski/Projects/rust/web-server/target/debug/deps/libweb_server-f281a6883c034ec8.rmeta: src/main.rs
+
+/home/pbielski/Projects/rust/web-server/target/debug/deps/web_server-f281a6883c034ec8.d: src/main.rs
+
+src/main.rs:
